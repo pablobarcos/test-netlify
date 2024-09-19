36c1892d1b334fd2597cda8ffbfe6b1b1f28bcea
Pablo Barcos <pablo_lanzarote10@hotmail.com>
2024-09-19 07:51:15 +0100

    test vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,11 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: netlify()
+  adapter: vercel()
 });
